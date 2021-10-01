0af2cc03fa2dfbb7fe1ad2c7623f4ee9f1b42086
Caleb Kim <kim.caleb10@gmail.com>
2021-10-01 12:07:04 -0700

    Edited Single Product Query and created basic Styles Query

--- db/index.js
+++ db/index.js
@@ -10,17 +10,61 @@ const pool = new Pool({
 const products = (callback) => {
   pool.query('SELECT * FROM products ORDER BY id LIMIT 200')
     .then((res) => {
-      console.log(res.rows[0]);
       callback(res.rows);
-      // pool.end();
     })
     .catch((err) => console.error(err));
+};
+
+const product = (id, callback) => {
+  pool.query(`SELECT * FROM products WHERE id = ${id}`)
+    .then((res) => {
+      pool.query(`SELECT * FROM features WHERE product_id = ${id}`)
+        .then((results) => {
+          res.rows[0].features = [];
+          results.rows.map((item) => res.rows[0].features.push({
+            feature: item.value,
+            value: item.feature,
+          }));
+          callback(res.rows[0]);
+        });
+    });
+};
+
+const styles = (id, callback) => {
+  pool.query(`SELECT * FROM styles WHERE product_id = ${id}`)
+    .then((style) => {
+      const styleIds = style.rows.map((item) => item.id);
+
+      let stylesObj = {
+        product_id: id,
+      };
+      let finalStyles = style.rows.map((item) => {
+        const resultsObj = {
+          style_id: item.id,
+          name: item.name,
+          original_price: item.original_price,
+          sale_price: item.sale_price,
+          default: item.default_style,
+        };
+        return resultsObj;
+      });
+      console.log(styleIds);
+      for (let i = 0; i < styleIds.length; i += 1) {
+        pool.query(`SELECT * FROM skus WHERE style_id = ${styleIds[i]}`)
+          .then((ps) => {
+            stylesObj.results[i].skus = ps.rows;
+            console.log(stylesObj.results);
+          });
+      }
+      stylesObj.results = finalStyles;
+      console.log(stylesObj);
+      callback(stylesObj);
+    });
 };
 
 const related = (id, callback) => {
   pool.query(`SELECT related_product_id FROM related WHERE current_product_id = ${id} ORDER BY related_product_id`)
     .then((res) => {
-      console.log(res.rows);
       const relatedArray = res.rows.map((item) => item.related_product_id);
       callback(relatedArray);
     })
@@ -29,5 +73,7 @@ const related = (id, callback) => {
 
 module.exports = {
   products,
+  product,
+  styles,
   related,
 };
--- server/server.js
+++ server/server.js
@@ -11,8 +11,17 @@ app.get('/products', (req, res) => {
   // res.send('Hello World');
 });
 
+app.get('/products/:product_id', (req, res) => {
+  index.product(req.params.product_id, (results) => res.send(results));
+});
+
+app.get('/products/:product_id/styles', (req, res) => {
+  index.styles(req.params.product_id, (results) => res.send(results));
+});
+
 app.get('/products/:product_id/related', (req, res) => {
   index.related(req.params.product_id, (results) => res.send(results));
 });
 
+// eslint-disable-next-line no-console
 app.listen(3000, console.log('Listening on Port 3000!'));
